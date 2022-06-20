26b1567d9cb837fa25389598d5f43c3d157e04e5
serag622 <45487732+serag622@users.noreply.github.com>
2022-06-20 15:31:22 +0200

    update

--- src/app/components/movies/movies.module.ts
+++ src/app/components/movies/movies.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MoviesListComponent } from './movies-list/movies-list.component';
-import { MovieCardComponent } from './movie-card/movie-card.component';
 import { MovieFormComponent } from './movie-form/movie-form.component';
 import {MoviesRoutingModule} from './movies-routing.module';
-// import { NgSelectModule } from '@ng-select/ng-select';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
@@ -12,13 +10,11 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     MoviesListComponent,
-    MovieCardComponent,
     MovieFormComponent
   ],
   imports: [
     CommonModule,
     MoviesRoutingModule,
-    // NgSelectModule,
     FormsModule,
     ReactiveFormsModule
     ]
